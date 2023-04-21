e356e190a2a82456032dcefa0c6f9bdcadead159
Jeremy Lee <jeremylee0615@gmail.com>
2023-04-20 17:50:58 -0700

    Displaying Usernames & installed cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; 
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,10 +29,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
-// Creat URL 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
