4ae5919b1c8f7dafcdde67f85760560e9bf9a4e4
Jeremy Lee <jeremylee0615@gmail.com>
2023-04-24 20:25:20 -0700

    Implemented cookie-sessions & hashed passwords. Organized by sepertating helperFunctions from main server codes

--- express_server.js
+++ express_server.js
@@ -1,103 +1,38 @@
 const express = require("express");
-var cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcryptjs");
+
 const app = express();
 const PORT = 8080; 
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['Lighthouse Labs'],
+  /// 24 hours limit ///
+  maxAge: 24 * 60 * 60 * 1000
+}))
+
+/// Helper Functions exports ///
+const {generateRandomString, newUser, userNow, userOnlyURLs, checkUrls, isOwner, checkAvailEmail, verifyInfo} = require('./helperFunctions'); 
 
 
 const urlDatabase = {
-  "b2xVn2": {
+  /*"b2xVn2": {
     longURL: "http://www.lighthouselabs.ca", 
     user_id: "abc123"},
   "9sm5xK": {
     longURL: "http://www.google.com", 
-    user_id: "abc123"}
+    user_id: "abc123"}*/
 };
 
 const users = {
-  exampleUserInfo: {
+  /*exampleUserInfo: {
     id: "ihateerrors",
     email: "why@isitnotdefined.com",
-    password: "man-I-hate-this",}
+    password: "man-I-hate-this",}*/
 };
-
-
-/// Generates 6 digits of Random Strings consisting numbers and letters ///
-const generateRandomString = () => {
-  let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let lowerCase = upperCase.toLowerCase();
-  let numeric = '1234567890';
-  let inputs = upperCase + lowerCase + numeric;
-  let result = '';
-  let combinationLength = 6;
-  for (let i = 0; i < combinationLength; i++) {
-    result += inputs.charAt(Math.round(Math.random() * inputs.length));
-  }
-  return result;
-};
-
-/// adding New user to users database ///
-const newUser = (user) => {
- const newUserId = generateRandomString ()
- /// storing the new generated user's id to the database /// 
- user.id = newUserId;
- users[newUserId] = user;
- return user;
-}
-
-/// checking Currently signed in user ///
-const userNow = (ids, users) => {
-  for (let user in users) {
-    if (ids === user) {
-      return users[user].id;
-    }
-  }
-};
-
-/// Only User can see their URLs ///
-const userOnlyURLs = (id, urlDatabase) => {
-  const userURL = {};
-  for (let key in urlDatabase) {
-    if (urlDatabase[key].user_id === id) {
-      userURL[key] = urlDatabase[key];
-    }
-  }
-  return userURL
-};
-
-/// checking if URLS are stored/created in the database ///
-const checkUrls = (id, urlDatabase) => {
-  return urlDatabase [id];
-};
-
-/// checking if the users are eligible to modify URLs (checking its theirs or not) ///
-const isOwner = (user, id, urlDatabase) => {
-  return user === urlDatabase[id].user_id
-}
-
-/// checking if email is available to use ///
-const checkAvailEmail = (email, users) => {
-  for (let key in users) {
-    if (users[key].email === email) {
-      return false;
-    }
-  }
-  return true;
-};
-
-/// checking if user's info associated with email matched in database ///
-const verifyInfo = (email, users) => {
-  for (let key in users) {
-    if (users[key].email === email) {
-      return users[key];
-    }
-  }
-  return undefined;
-};
-
 
 
 
@@ -106,9 +41,21 @@ app.get("/urls.json", (req, res) => {
 });
 
 
+
+//////////////////// Direct TinyApp Page ////////////////////
+app.get("/", (req, res) => {
+  const currentUser = userNow(req.session.user_id, users)
+  if (!currentUser) {
+    res.redirect("/login")
+  } 
+  res.redirect("/urls");
+});
+
+
+
 //////////////////// Register Page ////////////////////
 app.get("/register", (req, res) => {
-  const currentUser = userNow(req.cookies['user_id'], users)
+  const currentUser = userNow(req.session.user_id, users)
   const templateVars = { 
     currentUser: currentUser
   };
@@ -119,27 +66,30 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const userEmail = req.body.email;
   const userPassword = req.body.password;
-  
+  ///implemented hashed passwords///
+  const hashedPassword = bcrypt.hashSync(userPassword, 10)
   /// Missing email input ///
   if (userEmail === '') {
-    return  res.status(400).send("Email is missing! ⛔")
+    res.status(400).send("Email is missing! ⛔")
   /// Missing passward input ///
   } else if (userPassword === '') {
-    return  res.status(400).send("Password is missing! ⛔")
+    res.status(400).send("Password is missing! ⛔")
   /// Entered existing emails /// 
   } else if (!checkAvailEmail (userEmail, users)) {
-    return res.status(400).send ("Sorry, the email you registered is already in use 🚫 ") 
+    res.status(400).send ("Sorry, the email you registered is already in use 🚫 ") 
+  } else {
+    req.body.password = hashedPassword
   }
-  
-  addNew = newUser (req.body)
-  res.cookie ('user_id', addNew.id)
+  addNew = newUser (req.body, users)
+  req.session.user_id = addNew.id;
   res.redirect("/urls");
 });
 
-//////////////////// Login Page ////////////////////
 
+
+//////////////////// Login Page ////////////////////
 app.get("/login", (req, res) => {
-  const currentUser = userNow(req.cookies['user_id'], users)
+  const currentUser = userNow(req.session.user_id, users)
   const templateVars = { 
     currentUser: currentUser,
   };
@@ -153,10 +103,10 @@ app.post("/login", (req, res) => {
   if (verifyInfo (emailEntered, users)) { 
     const userPassword = verifyInfo(emailEntered, users).password;
     const userId = verifyInfo(emailEntered, users).id;
-    if (userPassword !== passwordEntered) {
+    if (!bcrypt.compareSync(passwordEntered, userPassword)) {
       res.status(400).send ('Oops... Something is not quite right. Please check your password! ⛔')
     } else {
-      res.cookie('user_id', userId)
+      req.session.user_id = userId;
       res.redirect("/urls")
     }  
   } else {
@@ -164,9 +114,11 @@ app.post("/login", (req, res) => {
   } 
 });
 
+
+
 //////////////////// URLs main Page ////////////////////
 app.get("/urls", (req, res) => {
-  const currentUser = userNow(req.cookies['user_id'], users)
+  const currentUser = userNow(req.session.user_id, users)
   if (!currentUser) {
     res.status(400).send("Please Log in or Register First! ⛔")
   }
@@ -182,7 +134,7 @@ app.get("/urls", (req, res) => {
 app.post("/urls", (req, res) => {
   const generatedURL = generateRandomString();
   const longURL = req.body.longURL;
-  const currentUser = userNow(req.cookies['user_id'], users);
+  const currentUser = userNow(req.session.user_id, users);
   urlDatabase[generatedURL] = {
     longURL: longURL,
     user_id: currentUser
@@ -191,9 +143,10 @@ app.post("/urls", (req, res) => {
 });
 
 
+
 //////////////////// Create New URL Page ////////////////////
 app.get("/urls/new", (req, res) => {
-  const currentUser = userNow(req.cookies['user_id'], users)
+  const currentUser = userNow(req.session.user_id, users)
   if (!currentUser) {
     res.redirect('/login');
   }
@@ -205,10 +158,12 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+
+
 //////////////////// After Creating new URL Page ////////////////////
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const currentUser = userNow(req.cookies['user_id'], users)
+  const currentUser = userNow(req.session.user_id, users)
   /// if users manually type URL links that does not exist /// 
   if (!urlDatabase[id]) {
     res.status(400).send("The URL link does not exist 🚫")
@@ -218,7 +173,7 @@ app.get("/urls/:id", (req, res) => {
     }
   if (checkUrls(id, urlDatabase)) {
     const longURL = urlDatabase[id].longURL;
-    const currentUser = userNow(req.cookies['user_id'], users)
+    const currentUser = userNow(req.session.user_id, users)
     const templateVars = { 
     id: id,
     longURL: longURL,
@@ -229,6 +184,8 @@ app.get("/urls/:id", (req, res) => {
   res.send ("URLs not found")
  };
 });
+
+
 
 //////////////////// Direct to the URL site ////////////////////
 app.get("/u/:id", (req, res) => {
@@ -241,15 +198,17 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
+
+
 /// editing URL function ///
 app.post("/urls/:id/edit", (req, res) => {
   /// Check for LogIn first to pass through the other conditions ///  
-  if (!req.cookies['user_id']) {
+  if (!req.session.user_id) {
     res.status(400).send("Please Log in or Register First! ⛔");
   } 
   const id = req.params.id;
   const longURL = req.body.longURL
-  const currentUser = userNow(req.cookies['user_id'], users)
+  const currentUser = userNow(req.session.user_id, users)
   if (!isOwner (currentUser, id, urlDatabase)) {
     res.status(400).send("Sorry, This URL does not belong to you to modify ⛔");
   } else {
@@ -258,14 +217,16 @@ app.post("/urls/:id/edit", (req, res) => {
   }
 });
 
+
+
 /// deleting URL function ///
 app.post("/urls/:id/delete", (req, res) => {
 /// Check for LogIn first to pass through the other conditions ///  
-  if (!req.cookies['user_id']) {
+  if (!req.session.user_id) {
     res.status(400).send("Please Log in or Register First! ⛔");
   } 
   const id = req.params.id;
-  const currentUser = userNow(req.cookies['user_id'], users)
+  const currentUser = userNow(req.session.user_id, users)
   if (!isOwner (currentUser, id, urlDatabase)) {
     res.status(400).send("Sorry, This URL does not belong to you to modify ⛔");
   } else {
@@ -274,9 +235,11 @@ app.post("/urls/:id/delete", (req, res) => {
   }
 });
 
+
+
 /// Log Out function ///
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect("/login");
 });
 
---
+++ helperFunctions.js
@@ -0,0 +1,77 @@
+
+/// Generates 6 digits of Random Strings consisting numbers and letters ///
+const generateRandomString = () => {
+  let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let lowerCase = upperCase.toLowerCase();
+  let numeric = '1234567890';
+  let inputs = upperCase + lowerCase + numeric;
+  let result = '';
+  let combinationLength = 6;
+  for (let i = 0; i < combinationLength; i++) {
+    result += inputs.charAt(Math.round(Math.random() * inputs.length));
+  }
+  return result;
+};
+
+/// adding New user to users database ///
+const newUser = (user, users) => {
+ const newUserId = generateRandomString ()
+ /// storing the new generated user's id to the database /// 
+ user.id = newUserId;
+ users[newUserId] = user;
+ return user;
+}
+
+/// checking Currently signed in user ///
+const userNow = (ids, users) => {
+  for (let user in users) {
+    if (ids === user) {
+      return users[user].id;
+    }
+  }
+};
+
+/// Only User can see their URLs ///
+const userOnlyURLs = (id, urlDatabase) => {
+  const userURL = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].user_id === id) {
+      userURL[key] = urlDatabase[key];
+    }
+  }
+  return userURL
+};
+
+/// checking if URLS are stored/created in the database ///
+const checkUrls = (id, urlDatabase) => {
+  return urlDatabase [id];
+};
+
+/// checking if the users are eligible to modify URLs (checking its theirs or not) ///
+const isOwner = (user, id, urlDatabase) => {
+  return user === urlDatabase[id].user_id
+}
+
+/// checking if email is available to use ///
+const checkAvailEmail = (email, users) => {
+  for (let key in users) {
+    if (users[key].email === email) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/// checking if user's info associated with email matched in database ///
+const verifyInfo = (email, users) => {
+  for (let key in users) {
+    if (users[key].email === email) {
+      return users[key];
+    }
+  }
+  return undefined;
+};
+
+
+
+module.exports = {generateRandomString, newUser, userNow, userOnlyURLs, checkUrls, isOwner, checkAvailEmail, verifyInfo}; 
